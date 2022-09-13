4da126df934c16ca76f21000b9b212500bc92d4c
MaheusSaggiorato <matheussaggiorato@gmail.com>
2022-09-12 22:20:37 -0300

    alterações de design e textos

--- script.js
+++ script.js
@@ -3,8 +3,60 @@ let title = document.getElementById("title");
 let paragraph = document.getElementById("paragraph");
 let thankImage = document.getElementById("thankImage");
 let selectedNote = document.getElementById("selectedNote");
-let userNote = null;
-let notes = document.querySelectorAll(".notes")
+let notes = document.querySelectorAll(".notes");
+let buttonSubmit = document.getElementById("buttonSubmit");
+let userNote = undefined;
+
+function changeOnSubmit() {
+
+    function changeSelectedNote() {
+        selectedNote.style.display = "block";
+        selectedNote.innerText = `You selected ${userNote} out of 5`;
+    }
+    changeSelectedNote();
+
+    function showThankImage() {
+        thankImage.style.display = "block";
+    }
+    showThankImage();
+
+    removeCircles();
+
+    function changeTitle() {
+        title.innerText = "Thank you!";
+        title.style.textAlign = "center";
+    }
+    changeTitle();
+
+    function changeParagraph() {
+        paragraph.innerText = "Thank you for taking the time to give us a rating. If you need further support, please don't hesitate to contact us!"
+        paragraph.style.textAlign = "center";
+    }
+    changeParagraph();
+
+    function removeCircles() {
+        circles.forEach((circles) => {
+            circles.style.display = "none";
+        })
+    }
+
+    function removeButton() {
+        buttonSubmit.style.display = "none";
+    }
+    removeButton();
+
+}
+
+
+function changeBackgroundNote() {
+
+    notes.forEach((notes) => {
+        notes.addEventListener("click", function (event) {
+            event.target.style.backgroundColor = "hsl(25, 97%, 53%)";
+        })
+    })
+}
+changeBackgroundNote();
 
 function changeUserNote() {
 
@@ -22,52 +74,8 @@ function changeUserNote() {
                 userNote = 5;
             }
 
-            console.log(userNote)
+            console.log(userNote);
         })
     })
 }
-changeUserNote()
-
-function changeOnSubmit() {
-
-    function changeSelectedNote() {
-        selectedNote.style.display = "block";
-        selectedNote.innerText = `You selected ${userNote} out of 5`;
-    }
-    changeSelectedNote();
-
-    function showThankImage() {
-        thankImage.style.display = "block";
-    }
-    showThankImage();
-
-    function removeCircles() {
-        circles.forEach((circles) => {
-            circles.style.display = "none";
-        })
-    }
-    removeCircles();
-
-    function changeTitle() {
-        title.innerText = "Thank you!";
-        title.style.textAlign = "center";
-    }
-    changeTitle();
-
-    function changeParagraph() {
-        paragraph.innerText = "We appreciate you taking the thime to give a rating. If you ever need more support, don't hesitate to get in touch!";
-        paragraph.style.textAlign = "center";
-    }
-    changeParagraph();
-}
-
-
-function changeBackgroundNote() {
-
-    notes.forEach((notes) => {
-        notes.addEventListener("click", function (event) {
-            event.target.style.backgroundColor = "#fff";
-        })
-    })
-}
-changeBackgroundNote()
+changeUserNote();
